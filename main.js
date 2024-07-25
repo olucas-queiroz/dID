f43439d89f548b8bfc6571aa328d42760dd3fba9
Lucas Queiroz <olucasqueiroz99@gmail.com>
2024-07-25 11:01:00 -0300

    Detectando produto

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var marca = document.getElementById("marca");
 var modelo = document.getElementById("modelo");
 var sistema = document.getElementById("sistema");
+var produto = document.getElementById("produto");
 
 const info = platform.parse(navigator.userAgent);
 
@@ -9,3 +10,4 @@ console.log(info);
 marca.textContent = info.manufacturer;
 modelo.textContent = info.model;
 sistema.textContent = info.os.family;
+produto.textContent = info.product;
