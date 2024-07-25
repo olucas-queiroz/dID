d8d0e44504fad06cbd61e3dcee6a96f55ae3ebdb
Lucas Queiroz <olucasqueiroz99@gmail.com>
2024-07-25 10:54:29 -0300

    ignorando check e adicionando lib platform

--- main.js
+++ main.js
@@ -1,30 +1,11 @@
-var texto = document.getElementById("r");
+var marca = document.getElementById("marca");
+var modelo = document.getElementById("modelo");
+var sistema = document.getElementById("sistema");
 
-function checkDevice() {
-  if (navigator.userAgent.match(/Android/i)) {
-    texto.textContent = "Acesso via dispositivo Android.";
-  } else if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
-    texto.textContent = "Acesso via dispositivo iOS.";
-  } else {
-    texto.textContent = "Acesso via computador.";
-  }
-}
-// checkDevice();
+const info = platform.parse(navigator.userAgent);
 
-// Obtém o user agent do navegador
-const userAgent = navigator.userAgent;
+console.log(info);
 
-// Verifica se o dispositivo é um Android
-if (userAgent.includes("Android")) {
-  // Extrai o nome do navegador
-  const browserName = userAgent.split(" ")[0];
-  texto.textContent = `Navegador: ${browserName}`;
-
-  // Extrai o sistema operacional (Android)
-  const os = userAgent.match(/Android\s([\d\.]+)/);
-  if (os) {
-    texto.textContent = `Sistema Operacional: Android ${os[1]}`;
-  }
-} else {
-  texto.textContent = "Não é um dispositivo Android.";
-}
+marca.textContent = info.manufacturer;
+modelo.textContent = info.model;
+sistema.textContent = info.os.family;
